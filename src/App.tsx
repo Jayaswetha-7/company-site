b53d3a88b03ca2df70a25caad6f52c75176fff93
Gladwin_T01 <gladwins@taphubs.org>
2025-02-04 17:44:10 +0530

    Configured Vercel Routing

--- src/App.tsx
+++ src/App.tsx
@@ -39,9 +39,6 @@ function App() {
               <>
                 <Navbar />
                 <LandingCard />
-                {/* <Hero />
-                <WhyNeedUs /> */}
-                {/* <Service /> */}
                 <About />
                 <NewService />
                 <Careers />
