b8b70c57f21408afbb0362c83b87bf75cd786338
Sarath-T02 <sarathtv@taphubs.org>
2025-02-04 11:50:06 +0530

    Refactor SubHero component: replace anchor with Link and update button text

--- src/components/SubHero.tsx
+++ src/components/SubHero.tsx
@@ -1,6 +1,6 @@
 import { ArrowRight } from "lucide-react";
-import { motion } from "framer-motion";
 import bgImage from "../assets/subheroimg2.jpg";
+import { Link } from "react-router-dom";
 function SubHero() {
   return (
     <section
@@ -8,18 +8,16 @@ function SubHero() {
       style={{ backgroundImage: `url(${bgImage})` }}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col sm:ms-[20vw] sm:mb-[10vw]">
-        <h1 className="sm:text-[80px] text-[30px] text-gray-900 font-bold font-mono  ">
+        <h1 className="sm:text-[80px] text-[30px] text-gray-900/50 font-bold italic font-mono  ">
           All your bussiness Solutions
         </h1>
-        <motion.a
-          href=""
-          className="inline-flex items-center sm:w-[10vw] w-[50%] px-8 py-4 bg-gradient-to-r from-blue-500 to-blue-600 text-white rounded-full font-semibold hover:from-blue-600 hover:to-blue-700 transition-all shadow-lg hover:shadow-xl"
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
+        <Link
+          to={"/about/lineofbusiness"}
+          className="inline-flex items-center sm:w-[12vw] w-[60%] px-8 py-4 bg-gradient-to-r from-blue-500 to-blue-600 text-white rounded-full font-semibold hover:from-blue-600 hover:to-blue-700 transition-all shadow-lg hover:shadow-xl"
         >
-          Know More
+          Explore More
           <ArrowRight className="ml-2 h-5 w-5" />
-        </motion.a>
+        </Link>
       </div>
     </section>
   );
