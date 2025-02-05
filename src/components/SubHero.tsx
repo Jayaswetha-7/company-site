054c22bd4126468e869ab3bed6638f8c9e953e33
Sarath-T02 <sarathtv@taphubs.org>
2025-02-05 15:15:59 +0530

    Update SubHero component with new background image and enhanced styling for better visual appeal

--- src/components/SubHero.tsx
+++ src/components/SubHero.tsx
@@ -1,19 +1,23 @@
 import { ArrowRight } from "lucide-react";
-import bgImage from "../assets/subheroimg2.jpg";
+import bgImage from "../assets/subheroimg3.jpg";
 import { Link } from "react-router-dom";
 function SubHero() {
   return (
     <section
-      className=" bg-cover bg-center select-none sm:h-[100%] h-[95%] sm:p-0 rounded-2xl content-center "
-      style={{ backgroundImage: `url(${bgImage})` }}
+      className=" bg-cover bg-center select-none sm:h-[100%] h-[95%] sm:p-0 rounded-2xl mx-auto min-w-screen "
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        backgroundBlendMode: "darken",
+      }}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col sm:ms-[20vw] sm:mb-[10vw] mb-[12vh] items-center sm:items-start ">
-        <h1 className="sm:text-[80px] text-[30px] text-[#3b3a39] font-bold  font-mono text-center sm:text-left ">
-          All your bussiness Solutions
+      <div className=" mx-auto flex h-[80%] flex-col  justify-center items-start  gap-0 text-center ml-[5vw]   ">
+        <h1 className="  text-white font-bold  font-mono  sm:w-[60%] sm:text-[4vh] md:text-[6vh] text-left ">
+          Your Business, <br /> Our Smartest Solutions
         </h1>
         <Link
           to={"/about/lineofbusiness"}
-          className="inline-flex items-center sm:w-[12vw] w-[65%] h-[5vh] px-8 py-4 bg-gradient-to-r from-blue-500 to-blue-600 text-white rounded-full font-semibold hover:from-blue-600 hover:to-blue-700 transition-all shadow-lg hover:shadow-xl sm:text-[100%] text-[60%]"
+          className="inline-flex md:min-w-[300px] px-[10%] h-[5vh]  lg:w-[15vw] bg-gradient-to-r from-blue-500 to-blue-600 text-white rounded-full font-semibold hover:from-blue-600 hover:to-blue-700 transition-all shadow-lg hover:shadow-xl items-center "
         >
           Explore More
           <ArrowRight className="ml-2 sm:h-5 sm:w-5 h-3 w-3" />
@@ -24,3 +28,4 @@ function SubHero() {
 }
 
 export default SubHero;
+
