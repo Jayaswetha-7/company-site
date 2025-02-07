26a70dd8b5e3ce3946c9f76049daea1096c68ad9
Sarath-T02 <sarathtv@taphubs.org>
2025-02-07 16:12:37 +0530

    Enhance Error404 component with Navbar and improved styling for better user experience

--- src/components/Pages/Error404.tsx
+++ src/components/Pages/Error404.tsx
@@ -1,10 +1,40 @@
+import { Link } from "react-router-dom";
+import Navbar from "../Navbar";
 
 const Error404 = () => {
   return (
     <div>
-      
+      <Navbar />
+      <div
+        style={{
+          backgroundColor: "#f5f0f5",
+          backgroundImage: "linear-gradient(0deg, #f5f0f5 0%, #a8e2ea 100%)",
+        }}
+        className="w-[100vw] bg-white h-[100vh] flex items-center justify-center"
+      >
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-[6vw] mb-5 font-SmoochSans">
+            Page Not Found
+          </h1>
+          <Link to={"/"}>
+            <button
+              className="bg-gradient-to-r from-cyan-400 via-blue-600 to-cyan-400 
+    text-white uppercase text-sm md:text-base lg:text-lg 
+    px-4 md:px-6 lg:px-8 py-3 m-2 rounded-xl 
+    shadow-lg transition-all duration-500 ease-in-out 
+    bg-[length:200%_auto] hover:bg-right-center 
+    hover:scale-105 hover:shadow-xl 
+    flex justify-center items-center 
+    w-full max-w-[250px] whitespace-nowrap"
+            >
+              Back To Home
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Error404
+
