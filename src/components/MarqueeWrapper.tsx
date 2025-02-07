6dcef1193017ccb184fc22efef0503fd4e4ecabe
Sarath-T02 <sarathtv@taphubs.org>
2025-02-07 09:26:39 +0530

    Update MarqueeWrapper component to enhance styling and adjust marquee speed

--- src/components/MarqueeWrapper.tsx
+++ src/components/MarqueeWrapper.tsx
@@ -3,12 +3,13 @@ import Marquee from "react-fast-marquee";
 function MarqueeWrapper() {
 
   return (
-    <div className="w-[100vw]">
-      <div className="text-white bg-gray-900 w-[100vw] font-SmoochSans italic font-bold px-2 py-1 text-center text-[1.1vw] m-10 whitespace-break-spaces my-unformatted-div hidden xl:block">
-        <Marquee>
-          Shaping the Future with Technology... Empowering Leadership with
-          Technology... Leading Through Technology...
+    <div className="w-[100vw] bg-gray-900 ">
+      <div className="text-white bg-gray-900 w-full font-SmoochSans italic font-bold  py-1 text-center text-[1.1vw] whitespace-break-spaces my-unformatted-div hidden xl:block">
+        <Marquee speed={100}>
+          Shaping the Future with Technology...... Empowering Leadership with
+          Technology...... Leading Through Technology......
         </Marquee>
+        
       </div>
     </div>
   );
