644126d0ead5a8a41fdfa1a5a38d032c3f45d46d
Gladwin_T01 <gladwins@taphubs.org>
2025-02-13 10:44:36 +0530

    Enhance LanguageSwitcher component to update document direction based on selected language

--- src/components/LanguageSwitch.tsx
+++ src/components/LanguageSwitch.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 const LanguageSwitcher: React.FC = () => {
   const { i18n } = useTranslation();
 
   const changeLanguage = (lang: string) => {
+    document.documentElement.dir = i18n.language === "ar" ? "rtl" : "ltr";
+
     i18n.changeLanguage(lang);
-   
   };
+  useEffect(() => {
+    document.documentElement.dir = i18n.language === "ar" ? "rtl" : "ltr";
+  }, [i18n.language]);
 
   return (
     <div className="ms-10">
@@ -17,16 +21,8 @@ const LanguageSwitcher: React.FC = () => {
           value={i18n.language}
           className="bg-white border border-gray-300 text-center px-1  py-1 outline-none rounded-2xl "
         >
-          <option
-            value="en"
-          >
-            En
-          </option>
-          <option
-            value="ar"
-          >
-            العربية
-          </option>
+          <option value="en">En</option>
+          <option value="ar">العربية</option>
         </select>
       </div>
     </div>
